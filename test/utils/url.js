16bfc12a9f33e98135c930f4738ba2f9757c69da
YevhenKap <kapelianovych.y.v@gmail.com>
2022-07-18 13:23:17 +0300

    Added handling scripts and styles. Added the Stream instance.

--- test/utils/url.js
+++ test/utils/url.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const {strictEqual} = require('assert');
 
-const {urlFromPath} = require('../../lib/utils/url.js');
+const {withURLSeparator} = require('../../lib/utils/url.js');
 
 describe('url module', function () {
-  describe('urlFromPath', function () {
+  describe('withURLSeparator', function () {
     it('should convert the FS file path to the public URL', function () {
       const filePath = path.join(path.sep, 'about', 'index.html');
 
-      strictEqual(urlFromPath(filePath), '/about/index.html');
+      strictEqual(withURLSeparator(filePath), '/about/index.html');
     });
   });
 });
