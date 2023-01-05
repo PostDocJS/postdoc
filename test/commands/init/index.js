1897a262805e80d0c630a3f20c32441e45c3e6b4
YevhenKap <kapelianovych.y.v@gmail.com>
2023-01-05 19:53:15 +0200

    Increase timeout for the first test of the `init` command. Fix the way of passing arguments to the `spawnSync` function. Add test for initialisation of a project under an existing directory.

--- test/commands/init/index.js
+++ test/commands/init/index.js
@@ -1,15 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-const {ok} = require('assert');
 const {spawnSync} = require('child_process');
+const {ok, strictEqual} = require('assert');
 
 const {it, describe} = require('mocha');
 
 const {Directory} = require('../../../lib/files.js');
 
 describe('init command', function () {
-  it('should should init the project in an empty directory', function () {
-    const {error, signal} = spawnSync('npx postdoc init __artifacts__', {cwd: __dirname, shell: true});
+  it('should init the project in an empty directory', function () {
+    this.timeout(5000);
+
+    const executablePath = path.resolve('bin', 'postdoc.js');
+
+    const {error, signal} = spawnSync(
+      'node',
+      [executablePath, 'init', '__artifacts__'],
+      {cwd: __dirname, shell: true}
+    );
 
     if (error) {
       throw error;
@@ -34,7 +42,13 @@ describe('init command', function () {
   });
 
   it('should exit early with a message if the destination directory is not empty', function () {
-    const {error, output} = spawnSync('npx postdoc init __artifacts__', {cwd: __dirname, shell: true});
+    const executablePath = path.resolve('bin', 'postdoc.js');
+
+    const {error, output} = spawnSync(
+      'node',
+      [executablePath, 'init', '__artifacts__'],
+      {cwd: __dirname, shell: true}
+    );
 
     if (error) {
       throw error;
@@ -48,6 +62,33 @@ describe('init command', function () {
         )
     );
 
-    fs.rmSync(path.resolve(__dirname, '__artifacts__'), {recursive: true, force: true});
+    fs.rmSync(path.resolve(__dirname, '__artifacts__'), {recursive: true});
+  });
+
+  it('should infer the directory name', function () {
+    this.timeout(5000);
+
+    const artifactsDirectory = path.resolve(__dirname, '__artifacts__');
+
+    fs.mkdirSync(artifactsDirectory);
+
+    const {error} = spawnSync(
+      'node',
+      [path.resolve('bin', 'postdoc.js'), 'init', '.'],
+      {cwd: artifactsDirectory, shell: true}
+    );
+
+    if (error) {
+      throw error;
+    }
+
+    const packageJson = fs.readFileSync(
+      path.join(artifactsDirectory, 'package.json'),
+      {encoding: 'utf8'}
+    );
+
+    const {name} = JSON.parse(packageJson);
+
+    strictEqual(name, '__artifacts__');    
   });
 });
