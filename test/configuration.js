c7a46f68dbb931cf24dbe00b8962d1bcf2c601bf
Yevhen Kapelianovych <kapelianovych.y.v@protonmail.com>
2023-03-20 21:00:39 +0200

    Test injecting values to the config from env.

--- test/configuration.js
+++ test/configuration.js
@@ -6,6 +6,7 @@ import mockRequire from 'mock-require';
 import {it, afterEach, describe} from 'mocha';
 
 import {resolveConfiguration} from '../lib/configuration/resolve.js';
+import {initializeConfiguration} from '../lib/configuration/index.js';
 
 const mockConfigurationFilesToLoad = () => {
   mockRequire(resolve('postdoc.conf.cjs'), {server: {port: 5698}});
@@ -13,9 +14,9 @@ const mockConfigurationFilesToLoad = () => {
   mockRequire(resolve('postdoc.json'), {server: {force: true}});
 };
 
-describe('Configuration module', function () {
-  describe('files priority', function () {
-    it('should load the ES module config file at first', async function () {
+describe('Configuration module', function() {
+  describe('files priority', function() {
+    it('should load the ES module config file at first', async function() {
       mockFs({
         'postdoc.conf.mjs': 'export default {server: {base: \'/root\'}}',
         'postdoc.conf.cjs': 'module.exports = {server: {port: 5698}}',
@@ -29,7 +30,7 @@ describe('Configuration module', function () {
       strictEqual(configuration.server.base, '/root');
     });
 
-    it('should load CommonJS config if ES module is absent', async function () {
+    it('should load CommonJS config if ES module is absent', async function() {
       mockFs({
         'postdoc.conf.cjs': 'module.exports = {server: {port: 5698}}',
         'postdoc.conf.js': 'module.exports = {server: {strictPort: true}}',
@@ -44,7 +45,7 @@ describe('Configuration module', function () {
       strictEqual(configuration.server.port, 5698);
     });
 
-    it('should load CommonJS config if package.json does not contain type: module', async function () {
+    it('should load CommonJS config if package.json does not contain type: module', async function() {
       mockFs({
         'postdoc.conf.js': 'module.exports = {server: {strictPort: true}}',
         'postdoc.json': '{"server": {"force": true}}',
@@ -58,9 +59,12 @@ describe('Configuration module', function () {
       strictEqual(configuration.server.strictPort, true);
     });
 
-    it('should load the config as ES module if package.json contains the type: module', async function () {
+    it('should load the config as ES module if package.json contains the type: module', async function() {
       mockFs({
-        'postdoc.conf.js': 'export default {server: {strictPort: true}}',
+        // Unfortunately, we must declare property for the next suite here, because Node caches
+        // first import and there is no way to invalidate the cache because it is not exposed yet.
+        // https://github.com/nodejs/modules/issues/307#issuecomment-480353529
+        'postdoc.conf.js': 'export default {server: {strictPort: true}, prop: "${PROP_ENV}"}',
         'postdoc.json': '{"server": {"force": true}}',
         'package.json': '{"name":"testing-file", "type":"module"}'
       });
@@ -70,7 +74,7 @@ describe('Configuration module', function () {
       strictEqual(configuration.server.strictPort, true);
     });
 
-    it('should load JSON config if no other types are present', async function () {
+    it('should load JSON config if no other types are present', async function() {
       mockFs({
         'postdoc.json': '{"server": {"force": true}}',
         'package.json': '{"name":"testing-file"}'
@@ -83,9 +87,29 @@ describe('Configuration module', function () {
       strictEqual(configuration.server.force, true);
     });
 
-    afterEach(function () {
+    afterEach(function() {
       mockFs.restore();
       mockRequire.stopAll();
+    });
+  });
+
+  describe('Injecting environment variables', function() {
+    it('should replace the interpolation pattern with a environment variable with the pattern as the name', async function() {
+      mockFs({
+        '.env': 'PROP_ENV = "cool secret value"',
+        // There is no point in declaring content of the file here because it is never used.
+        // See comment above.
+        'postdoc.conf.js': '',
+        'package.json': '{"type":"module"}'
+      });
+    
+      const configuration = await initializeConfiguration();
+
+      strictEqual(configuration.prop, 'cool secret value');
+    });
+
+    afterEach(function() {
+      mockFs.restore();
     });
   });
 });
