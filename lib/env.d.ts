952cde33c9c3d2087f10e02cb88166787d971b41
Yevhen Kapelianovych <kapelianovych.y.v@protonmail.com>
2023-04-07 21:45:20 +0300

    Add the preview command. Split Vite configurations for build, dev and preview modes.

--- lib/env.d.ts
+++ lib/env.d.ts
@@ -2,6 +2,9 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			readonly MODE: string;
+			readonly BROWSER: string;
+			/** Space separated list of arguments. */
+			readonly BROWSER_ARGS: string;
 		}
 	}
 }
