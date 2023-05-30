310c4c68af76dba0f8f8a18b39e24c936bc469e2
Shtymak <vova.shtimak@gmail.com>
2023-05-30 10:00:43 +0300

    Add test should load CommonJS config

--- test/src/configuration.js
+++ test/src/configuration.js
@@ -5,12 +5,12 @@ import {resolveConfigWithDir} from '../../lib/configuration/resolve.js';
 
 describe('Configuration module', function() {
   let tmpDir;
-  before(async function (client, done) {
+  beforeEach(async function (client, done) {
     tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'test-doc'));
     done();
   });
 
-  after(async function (client, done) {
+  afterEach(async function (client, done) {
     await fs.remove(tmpDir);
     done();
   });
@@ -20,11 +20,21 @@ describe('Configuration module', function() {
       await fs.writeFile(path.join(tmpDir, 'postdoc.conf.mjs'), 'export default {server: {base: \'/root\'}}');
       await fs.writeFile(path.join(tmpDir, 'postdoc.conf.cjs'), 'module.exports = {server: {port: 5698}}');
       await fs.writeFile(path.join(tmpDir, 'postdoc.conf.js'), 'module.exports = {server: {strictPort: true}}');
-      await fs.writeFile(path.join(tmpDir, 'postdoc.json'), '{"server": {"force": true}}');
+      await fs.writeFile(path.join(tmpDir, 'postdoc.json'), '{"server": {"force: true}}');
       await fs.writeFile(path.join(tmpDir, 'package.json'), '{"name":"testing-file"}');
       const {configuration} = await resolveConfigWithDir(tmpDir);
 
       client.assert.equal(configuration.server.base, '/root');
+    });
+    it('should load CommonJS config if ES module is absent', async function(client) {
+      await fs.writeFile(path.join(tmpDir, 'postdoc.conf.cjs'), 'module.exports = {server: {port: 5698}}');
+      await fs.writeFile(path.join(tmpDir, 'postdoc.conf.js'), 'module.exports = {server: {strictPort: true}}');
+      await fs.writeFile(path.join(tmpDir, 'postdoc.json'), '{"server": {"force: true}}');
+      await fs.writeFile(path.join(tmpDir, 'package.json'), '{"name":"testing-file"}');
+
+      const {configuration} = await resolveConfigWithDir(tmpDir);
+
+      client.assert.equal(configuration.server.port, 5698);
     });
   });
 });
