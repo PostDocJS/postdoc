c36ce9a8a0275d7642bafec8dd788415ac306950
Shtymak <vova.shtimak@gmail.com>
2023-06-13 11:36:04 +0300

    Move container set to before & add tests

--- test/src/bundler/page/index.js
+++ test/src/bundler/page/index.js
@@ -17,7 +17,7 @@ describe('page', function() {
   beforeEach(async function(client, done) {
     oldCwd = process.cwd();
     tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'test-index'));
-
+    Container.set(CONFIGURATION_ID, defaultConfiguration);
     await fs.mkdir(path.join(tmpDir, 'pages'));
     process.chdir(tmpDir);
     done();
@@ -57,7 +57,7 @@ describe('page', function() {
 
   
   it('should create a page which consists of a named layout file and a scoped content file', async function(client) {
-    Container.set(CONFIGURATION_ID, defaultConfiguration);
+   
     await fs.writeFile(path.join('pages', 'foo.layout.ejs'), 'content'); 
     await fs.writeFile(path.join('pages', 'foo.md'), 'content');
     const pages = getAllPages(defaultConfiguration);
@@ -70,6 +70,64 @@ describe('page', function() {
   });
   
   
+  it('should create a page which consist of a scoped layout file and a scoped content file', async function(client) {
 
+    await fs.mkdir(path.join('pages', 'foo'));
+    await fs.writeFile(path.join('pages', 'foo', 'index.layout.ejs'), 'layout');
+    await fs.writeFile(path.join('pages', 'foo', 'index.md'), 'content');
+
+    const pages = getAllPages(defaultConfiguration);
+
+    client.assert.strictEqual(pages.length, 1);
+    client.assert.strictEqual(pages[0].url, '/foo/index.html');
+    client.assert.ok(pages[0].content.exists());
+    client.assert.strictEqual(await pages[0].content.content(), 'content');
+    client.assert.strictEqual(pages[0].output.source(), path.join('out', 'foo', 'index.html'));
+  });
+
+  it('should not pick up a named content file if the layout file is scoped', async function(client) {
+
+    await fs.mkdir(path.join('pages', 'foo'));
+    await fs.writeFile(path.join('pages', 'foo', 'index.layout.ejs'), 'layout');
+    await fs.writeFile(path.join('pages', 'foo.md'), 'content');
+    await fs.writeFile(path.join('pages', 'foo', 'index.md'), 'content');
+
+    const pages = getAllPages(defaultConfiguration);
+    client.assert.strictEqual(pages.length, 1);
+    client.assert.strictEqual(pages[0].url, '/foo/index.html');
+    client.assert.ok(pages[0].content.exists());
+    client.assert.strictEqual(pages[0].output.source(), path.join('out', 'foo', 'index.html'));
+  });
+
+  it('should create a root index page with a root index layout and content file', async function(client) {
+  
+
+    await fs.writeFile(path.join('pages', 'index.layout.ejs'), 'layout');
+    await fs.writeFile(path.join('pages', 'index.md'), 'content');
+
+    const pages = getAllPages(defaultConfiguration);
+
+    client.assert.strictEqual(pages.length, 1);
+    client.assert.strictEqual(pages[0].url, '/index.html');
+    client.assert.ok(pages[0].content.exists());
+    client.assert.strictEqual(await pages[0].content.content(), 'content');
+    client.assert.strictEqual(pages[0].output.source(), path.join('out', 'index.html'));
+  });
+
+  it('should not pick up a scoped content file for the root index layout file', async function(client) {
+
+    await fs.mkdir(path.join('pages', 'index'));
+    await fs.writeFile(path.join('pages', 'index.layout.ejs'), 'layout');
+    await fs.writeFile(path.join('pages', 'index', 'index.md'), 'content');
+
+
+    const pages = getAllPages(defaultConfiguration);
+
+    client.assert.strictEqual(pages.length, 1);
+    client.assert.strictEqual(pages[0].url, '/index/index.html');
+    client.assert.ok(pages[0].content.exists());
+
+    client.assert.strictEqual(pages[0].output.source(), path.join('out', 'index', 'index.html'));
+  });
 
 });
