8a41e984003b077028be3ae6d8098cc4fcf86cac
ClimenteA <climente.alin@gmail.com>
2024-03-19 14:05:11 +0200

    feat: pagefind search

--- client/index.js
+++ client/index.js
@@ -78,3 +78,16 @@ const _session = new Session();
 globalThis.PostDoc ??= _session
 
 export const session = _session;
+
+
+export async function navigateTo(url, replace) {
+    await globalThis.PostDoc.navigator.navigateTo(url, replace);
+}
+
+export function onLeave(callback, options) {
+    globalThis.PostDoc.navigator.registerOnLeaveCallback(callback, options);
+}
+
+export function onRender(callback, options) {
+    globalThis.PostDoc.navigator.registerOnRenderCallback(callback, options);
+}
